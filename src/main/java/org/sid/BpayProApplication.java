cbe2646648d811721f33fcc03977ed9003f836dd
Salma <salmabac23@gmail.com>
2019-04-08 11:49:18 +0100

    push operation

--- src/main/java/org/sid/BpayProApplication.java
+++ src/main/java/org/sid/BpayProApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BpayProApplication {
 
+	//test push git
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BpayProApplication.class, args);
 	}
